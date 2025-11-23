e2e9f76a442db5bedd35ad73c42daf4a2da505a8
MeTaLdiCODING <daniilterehov353@gmail.com>
2025-11-23 14:32:06 +0300

    Moved the course values to improve code readability

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ convert(takeInitial(),takeExchange(),getAmount());
         System.out.println("EUR");
         Scanner initialMoney = new Scanner(System.in);
         return initialMoney. next().toUpperCase();
-
     }
     static String takeExchange(){
         System.out.println("Выберете на какую валюту вы хотите конвертировать: ");
@@ -19,7 +18,6 @@ convert(takeInitial(),takeExchange(),getAmount());
         System.out.println("EUR");
         Scanner ExchangeMoney = new Scanner(System.in);
         return ExchangeMoney.next().toUpperCase();
-
     }
     static double getAmount(){
         System.out.println("Введите сумму которую вы хотите конвертировать: ");
@@ -27,14 +25,18 @@ convert(takeInitial(),takeExchange(),getAmount());
         return amountMoney.nextDouble();
     }
     static void convert(String takeInitial, String takeExchange, double getAmount){
-
-
+        double RUB_to_USD=0.012654;
+        double RUB_to_EUR=0.011042;
+        double USD_to_RUB= 79.02;
+        double USD_to_EUR=0.8685;
+        double EUR_to_USD=1.15;
+        double EUR_to_RUB=90.56;
 if(takeInitial.equals("RUB")){
     if(takeExchange.equals("USD")){
-    System.out.println("Ваша сумма в USD: " + getAmount* 0.012654);
+    System.out.println("Ваша сумма в USD: " + getAmount* RUB_to_USD);
     }
     else if(takeExchange.equals("EUR")){
-    System.out.println("Ваша сумма в EUR: "+getAmount* 0.011042);
+    System.out.println("Ваша сумма в EUR: "+getAmount* RUB_to_EUR);
     }
     else{
         System.out.println("Введено некоректное значение!");
@@ -42,10 +44,10 @@ if(takeInitial.equals("RUB")){
 }
         if(takeInitial.equals("USD")){
             if(takeExchange.equals("RUB")){
-                System.out.println("Ваша сумма в RUB: " + getAmount* 79.02);
+                System.out.println("Ваша сумма в RUB: " + getAmount* USD_to_RUB);
             }
             else if(takeExchange.equals("EUR")){
-                System.out.println("Ваша сумма в EUR: "+getAmount* 0.8685);
+                System.out.println("Ваша сумма в EUR: "+getAmount* USD_to_EUR);
             }
             else{
                 System.out.println("Введено некоректное значение!");
@@ -53,16 +55,14 @@ if(takeInitial.equals("RUB")){
         }
         if(takeInitial.equals("EUR")){
             if(takeExchange.equals("USD")){
-                System.out.println("Ваша сумма в USD: " + getAmount* 1.15);
+                System.out.println("Ваша сумма в USD: " + getAmount* EUR_to_USD);
             }
             else if(takeExchange.equals("RUB")){
-                System.out.println("Ваша сумма в RUB: "+getAmount* 90.56);
+                System.out.println("Ваша сумма в RUB: "+getAmount* EUR_to_RUB);
             }
             else{
                 System.out.println("Введено некоректное значение!");
             }
         }
-
-
     }
 }
